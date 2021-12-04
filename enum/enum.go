10ce7a93f4fa32c8ab3c23351472a71927737c56
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-04 11:08:16 -0500

    Add enum.FlatMap

--- enum/enum.go
+++ enum/enum.go
@@ -141,11 +141,36 @@ func FindWithIndex[T any](values []T, f FilterFuncWithIndex[T], defValue T) T {
 	return defValue
 }
 
+// FlatMap maps the given f(value) and flattens the result.
+// For example:
+//   FlatMap([]int{1,2,3}, func (v int) []string {
+//     s := fmt.Sprintf("%v", v)
+//     return []string{s,s}
+//   })
+// returns:
+//   []string{"1","1","2","2","3","3"}
+func FlatMap[S any, T any](values []S, f func(S) []T) []T {
+	result := []T{}
+	for _, v := range values {
+		result = append(result, f(v)...)
+	}
+	return result
+}
+
+// FlatMapWithIndex maps the given f(index, value) and flattens the result.
+func FlatMapWithIndex[S any, T any](values []S, f func(int, S) []T) []T {
+	result := []T{}
+	for i, v := range values {
+		result = append(result, f(i, v)...)
+	}
+	return result
+}
+
 // Map maps a slice of values from one type to another
 // using the provided func f.
-func Map[S any, T any](slice []T, f func(a T) S) []S {
-	result := make([]S, 0, len(slice))
-	for _, v := range slice {
+func Map[S any, T any](values []T, f func(a T) S) []S {
+	result := make([]S, 0, len(values))
+	for _, v := range values {
 		result = append(result, f(v))
 	}
 	return result
--- enum/enum_test.go
+++ enum/enum_test.go
@@ -1,11 +1,42 @@
 package enum
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/must"
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestFlatMap_IntToString(t *testing.T) {
+	tests := []struct {
+		name   string
+		values []int
+		f      func(int) []string
+		want   []string
+	}{
+		{
+			name:   "empty int to string",
+			values: []int{},
+			want:   []string{},
+		},
+		{
+			name:   "doc example",
+			values: []int{1, 2, 3},
+			f:      func(v int) []string { s := fmt.Sprintf("%v", v); return []string{s, s} },
+			want:   []string{"1", "1", "2", "2", "3", "3"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := FlatMap(tt.values, tt.f)
+			if !cmp.Equal(got, tt.want) {
+				t.Errorf("FlatMap(%+v) = %+v, want %+v", tt.values, got, tt.want)
+			}
+		})
+	}
+}
 
 func TestMap_StringToInt(t *testing.T) {
 	tests := []struct {
