6f3ed079b3169a4647625bd7934444a32ed151bc
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-05 15:45:19 -0500

    Add stream.Repeatedly

--- stream/stream_test.go
+++ stream/stream_test.go
@@ -2,6 +2,7 @@ package stream
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/enum"
@@ -219,5 +220,15 @@ func TestMap_StringToInt(t *testing.T) {
 				t.Errorf("Map(%+v) = %+v, want %+v", tt.from, got, tt.want)
 			}
 		})
+	}
+}
+
+func TestRepeatedly(t *testing.T) {
+	rand.Seed(0)
+	want := []int32{2029793274, 526058514, 1408655353, 116702506, 789387515,
+		621654496, 413258767, 1407315077, 1926657288, 359390928}
+	got := Take(Repeatedly(rand.Int31), 10)
+	if !cmp.Equal(got, want) {
+		t.Errorf("Repeatedly = %+v, want %+v", got, want)
 	}
 }
