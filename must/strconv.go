3cf297eafad8e8f996da202c84389ebbd92a1a27
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-04 20:06:54 -0500

    Add strconv functions

--- must/strconv.go
+++ must/strconv.go
@@ -14,12 +14,52 @@ func Atoi(s string) int {
 	return v
 }
 
-// ParseInt parses an integer with the given base and size.
+// ParseBool parses a boolean.
 // It dies if there is an error.
-func ParseInt(s string, base, size int) int {
-	v, err := strconv.ParseInt(s, base, size)
+func ParseBool(str string) bool {
+	v, err := strconv.ParseBool(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return v
+}
+
+// ParseComplex parses a complex number with the given bitSize.
+// It dies if there is an error.
+func ParseComplex(s string, bitSize int) complex128 {
+	v, err := strconv.ParseComplex(s, bitSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return v
+}
+
+// ParseFloat parses a complex number with the given bitSize.
+// It dies if there is an error.
+func ParseFloat(s string, bitSize int) float64 {
+	v, err := strconv.ParseFloat(s, bitSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return v
+}
+
+// ParseInt parses an integer with the given base and bitSize.
+// It dies if there is an error.
+func ParseInt(s string, base, bitSize int) int {
+	v, err := strconv.ParseInt(s, base, bitSize)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return int(v)
+}
+
+// ParseUint parses an unsigned integer with the given base and bitSize.
+// It dies if there is an error.
+func ParseUint(s string, base int, bitSize int) uint64 {
+	v, err := strconv.ParseUint(s, base, bitSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return v
 }
