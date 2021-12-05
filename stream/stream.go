1d7e3f385a4a23b59db8ebe124c5229afb9ed9c7
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-05 14:59:59 -0500

    Add stream.ChunkEvry

--- stream/stream.go
+++ stream/stream.go
@@ -4,6 +4,7 @@ package stream
 
 import (
 	"constraints"
+	"log"
 )
 
 // First returns the first item of the provided chan or its zero value.
@@ -82,6 +83,30 @@ func Count[T any](ch <-chan T, f func(T) bool) int {
 		}
 	}
 	return result
+}
+
+// ChunkEvery takes a channel of values and chunks them n-at-a-time
+// with the given step size. It discards any left-over items.
+func ChunkEvery[T any](ch <-chan T, n, step int) <-chan []T {
+	if step > n {
+		log.Fatalf("step(%v) must be <= n(%v)", step, n)
+	}
+
+	outCh := make(chan []T, defaultBufSize)
+
+	go func() {
+		chunk := make([]T, 0, n)
+		for v := range ch {
+			chunk = append(chunk, v)
+			if len(chunk) == n {
+				outCh <- chunk
+				chunk = chunk[step:]
+			}
+		}
+		close(outCh)
+	}()
+
+	return outCh
 }
 
 // CountWithIndex returns the count of items in the channel for which
