418359cbe9248238c493a49aaa826e52e1040f55
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-08 09:38:34 -0500

    day08 solved

--- day08/part2/main.go
+++ day08/part2/main.go
@@ -42,16 +42,16 @@ func solveLine(line string) int {
 	lhs := Map(strings.Split(parts[0], " "), sortWord)
 	rhs := Map(strings.Split(parts[1], " "), sortWord)
 	all := append(append([]string{}, lhs...), rhs...)
-	logf("lhs=%v, rhs=%v", lhs, rhs)
+	// logf("lhs=%v, rhs=%v", lhs, rhs)
 
 	digits := Reduce(all, str2digit{}, identify147)
 	digBits := Reduce(all, str2bits{}, strToBits)
 	if All(rhs, maps.HasKey(digits)) {
 		v := ReduceWithIndex(rhs, 0, calcNum(digits))
-		logf("digits=%+v, value=%v", digits, v)
+		// logf("digits=%+v, value=%v", digits, v)
 		return v
 	}
-	logf("digits=%+v, digBits=%+v", digits, digBits)
+	// logf("digits=%+v, digBits=%+v", digits, digBits)
 
 	swapDigits := maps.Swap(digits)
 	swapDigBits := maps.Swap(digBits)
@@ -61,7 +61,6 @@ func solveLine(line string) int {
 	four := digBits[swapDigits[4]]
 	seven := digBits[swapDigits[7]]
 	a := one ^ seven
-	logf("mask=0x%02x, a=0x%02x, one=0x%02x, four=0x%02x, seven=0x%02x", mask, a, one, four, seven)
 	ll2 := mask &^ (four | seven)
 	e := byte(1 << bits.TrailingZeros(uint(ll2)))
 	g := ll2 ^ e
@@ -78,7 +77,6 @@ func solveLine(line string) int {
 	nine = four | seven | g
 	digits[swapDigBits[nine]] = 9
 	swapDigits[9] = swapDigBits[nine]
-	logf("nine=0x%02x, digits[%v]=%v", nine, swapDigits[9], digits[swapDigits[9]])
 
 	var three byte
 	if _, ok := swapDigBits[nine^b]; !ok {
@@ -87,7 +85,6 @@ func solveLine(line string) int {
 	three = nine ^ b
 	digits[swapDigBits[three]] = 3
 	swapDigits[3] = swapDigBits[three]
-	logf("three=0x%02x, digits[%v]=%v", three, swapDigits[3], digits[swapDigits[3]])
 
 	var five byte
 	if _, ok := swapDigBits[nine^c]; !ok {
@@ -96,24 +93,22 @@ func solveLine(line string) int {
 	five = nine ^ c
 	digits[swapDigBits[five]] = 5
 	swapDigits[5] = swapDigBits[five]
-	logf("five=0x%02x, digits[%v]=%v", five, swapDigits[5], digits[swapDigits[5]])
 
 	zero := a | b | c | e | f | g
 	digits[swapDigBits[zero]] = 0
 	swapDigits[0] = swapDigBits[zero]
-	logf("zero=0x%02x, digits[%v]=%v", zero, swapDigits[0], digits[swapDigits[0]])
 
 	two := a | c | d | e | g
 	digits[swapDigBits[two]] = 2
 	swapDigits[2] = swapDigBits[two]
-	logf("two=0x%02x, digits[%v]=%v", two, swapDigits[2], digits[swapDigits[2]])
 
 	six := a | b | d | e | f | g
 	digits[swapDigBits[six]] = 6
 	swapDigits[6] = swapDigBits[six]
-	logf("six=0x%02x, digits[%v]=%v", six, swapDigits[6], digits[swapDigits[6]])
 
-	return 0
+	v := ReduceWithIndex(rhs, 0, calcNum(digits))
+	// logf("digits=%+v, value=%v", digits, v)
+	return v
 }
 
 type str2digit map[string]int
