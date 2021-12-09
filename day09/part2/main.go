3ac109ab0274a5621778083656b93003728c08b9
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-09 01:09:19 -0500

    wip

--- day09/part2/main.go
+++ day09/part2/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"sort"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/maps"
@@ -31,22 +32,69 @@ func process(filename string) {
 		return acc
 	})
 
-	risk := maps.Reduce(m, 0, riskLevel(m))
+	lowPts := maps.Reduce(m, []keyT{}, findLowPoints(m))
+	logf("lowPts=%+v", lowPts)
+	visited := map[keyT]bool{}
+	allBasins := Map(lowPts, calcBasinSize(m, visited))
+	logf("allBasins=%+v", allBasins)
+	sort.Sort(sort.Reverse(sort.IntSlice(allBasins)))
+	answer := Product(allBasins[0:3])
 
-	printf("Solution: %v\n", risk)
+	// 791200 is wrong. = 100 * 92 * 86
+
+	printf("Solution: %v\n", answer)
 }
 
 type gridT map[keyT]int
 
 type keyT struct{ x, y int }
 
-func riskLevel(m gridT) func(k keyT, v, acc int) int {
+func calcBasinSize(m gridT, visited map[keyT]bool) func(k keyT) int {
+	return func(k keyT) int {
+		v := checkNeighbors(m, k, visited)
+		if v == 100 || v == 92 || v == 86 {
+			logf("v=%v, lowPt=%v", v, k)
+		}
+		return v
+	}
+}
+
+func checkNeighbors(m gridT, k keyT, visited map[keyT]bool) int {
+	if visited[k] {
+		return 0
+	}
+	visited[k] = true
+	if m[k] == 9 {
+		return 0
+	}
+	acc := 1
+	if p := right(k); m[p] == 1+m[k] {
+		acc += checkNeighbors(m, p, visited)
+	}
+	if p := left(k); m[p] == 1+m[k] {
+		acc += checkNeighbors(m, p, visited)
+	}
+	if p := up(k); m[p] == 1+m[k] {
+		acc += checkNeighbors(m, p, visited)
+	}
+	if p := down(k); m[p] == 1+m[k] {
+		acc += checkNeighbors(m, p, visited)
+	}
+	return acc
+}
+
+func right(k keyT) keyT { return keyT{x: k.x + 1, y: k.y} }
+func left(k keyT) keyT  { return keyT{x: k.x - 1, y: k.y} }
+func up(k keyT) keyT    { return keyT{x: k.x, y: k.y - 1} }
+func down(k keyT) keyT  { return keyT{x: k.x, y: k.y + 1} }
+
+func findLowPoints(m gridT) func(k keyT, v int, acc []keyT) []keyT {
 	d := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-	return func(k keyT, v, acc int) int {
+	return func(k keyT, v int, acc []keyT) []keyT {
 		if !All(d, isLowPoint(m, k, v)) {
 			return acc
 		}
-		return acc + v + 1
+		return append(acc, k)
 	}
 }
 
