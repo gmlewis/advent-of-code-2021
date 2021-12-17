8a010d1f03b2f0bb7523cf7babaeb2e7bfb2aa01
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-17 13:45:50 -0500

    Add day17

--- day17/part1/main.go
+++ day17/part1/main.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -23,6 +25,119 @@ func main() {
 func process(filename string) {
 	logf("Processing %v ...", filename)
 	buf := must.ReadFile(filename)
+	parts := strings.Split(buf, "=")
+	xp := strings.Split(strings.TrimSuffix(parts[1], ", y"), "..")
+	yp := strings.Split(parts[2], "..")
+	xmin := must.Atoi(xp[0])
+	xmax := must.Atoi(xp[1])
+	ymin := must.Atoi(yp[0])
+	ymax := must.Atoi(yp[1])
+	logf("target area: x=%v..%v, y=%v..%v", xmin, xmax, ymin, ymax)
 
-	printf("Solution: %v\n", len(buf))
+	visited := map[keyT]bool{}
+	maxHeight := math.MinInt
+	for steps := 1; steps < 1000; steps++ {
+		height, ok := calcHeight(steps, xmin, xmax, ymin, ymax, visited)
+		if !ok {
+			continue
+		}
+		// logf("steps=%v, height=%v", steps, height)
+		if height < maxHeight {
+			// logf("breaking at steps=%v", steps)
+			break
+		}
+		maxHeight = height
+	}
+
+	printf("Solution: %v\n", maxHeight)
+}
+
+type keyT [2]int
+
+func shoot(steps, xmin, ymin, xmax, ymax int) (xvMin, yvMin, xvMax, yvMax int) {
+	xvMin = math.MaxInt
+	for xv := 1; true; xv++ {
+		x := xv * (xv + 1) / 2
+		if steps < xv {
+			d := xv - steps
+			x -= d * (d + 1) / 2
+		}
+		if x >= xmin && xv < xvMin {
+			xvMin = xv
+		}
+		if x > xmax {
+			break
+		}
+		xvMax = xv
+	}
+
+	sum := steps * (steps - 1) / 2
+	yvMin = int(math.Round(float64(ymin+sum) / float64(steps)))
+	yvMax = int(math.Round(float64(ymax+sum) / float64(steps)))
+	if yvMin > yvMax {
+		yvMin, yvMax = yvMax, yvMin
+	}
+	return xvMin, yvMin, xvMax, yvMax
+}
+
+func calcHeight(steps, xmin, xmax, ymin, ymax int, visited map[keyT]bool) (int, bool) {
+	xv1, yv1, xv2, yv2 := shoot(steps, xmin, ymin, xmax, ymax)
+
+	maxHeight := math.MinInt
+	var foundOne bool
+	// var bestXV, bestYV int
+	for yv := yv1; yv <= yv2; yv++ {
+		for xv := xv1; xv <= xv2; xv++ {
+			k := keyT{xv, yv}
+			if visited[k] {
+				continue
+			}
+			visited[k] = true
+			height, ok := simulate(xv, yv, xmin, xmax, ymin, ymax)
+			if !ok {
+				continue
+			}
+			foundOne = true
+			if height > maxHeight {
+				maxHeight = height
+				// bestXV, bestYV = xv, yv
+			}
+		}
+	}
+	// if foundOne {
+	// 	logf("calcHeight(steps=%v): best initial velocity: (%v,%v) with height %v", steps, bestXV, bestYV, maxHeight)
+	// }
+
+	return maxHeight, foundOne
+}
+
+// simulate performs the following actions after each step:
+// The probe's x position increases by its x velocity.
+// The probe's y position increases by its y velocity.
+// Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
+// Due to gravity, the probe's y velocity decreases by 1.
+func simulate(xv, yv, xmin, xmax, ymin, ymax int) (int, bool) {
+	// initXV, initYV := xv, yv
+	xp, yp := 0, 0
+	maxHeight := 0 // starting height
+	var steps int
+	for xp <= xmax && yp >= ymin {
+		xp += xv
+		yp += yv
+		if xv > 0 {
+			xv--
+		}
+		yv--
+		if yp > maxHeight {
+			maxHeight = yp
+		}
+		steps++
+		// logf("after step #%v: pos=(%v,%v), vel=(%v,%v)", steps, xp, yp, xv, yv)
+
+		if xp >= xmin && xp <= xmax && yp >= ymin && yp <= ymax {
+			// logf("simulate found solution after %v steps with initial velocity (%v,%v)", steps, initXV, initYV)
+			return maxHeight, true
+		}
+	}
+	return 0, false
 }
--- day17/part1/main_test.go
+++ day17/part1/main_test.go
@@ -6,8 +6,189 @@ import (
 	"github.com/gmlewis/advent-of-code-2021/test"
 )
 
+func TestShoot(t *testing.T) {
+	tests := []struct {
+		name      string
+		steps     int
+		wantXVMin int
+		wantYVMin int
+		wantXVMax int
+		wantYVMax int
+	}{
+		{
+			name:      "1 step",
+			steps:     1,
+			wantXVMin: 20,
+			wantYVMin: -10,
+			wantXVMax: 30,
+			wantYVMax: -5,
+		},
+		{
+			name:      "2 steps",
+			steps:     2,
+			wantXVMin: 11,
+			wantYVMin: -5,
+			wantXVMax: 15,
+			wantYVMax: -2,
+		},
+		{
+			name:      "first example: 7 steps",
+			steps:     7,
+			wantXVMin: 6,
+			wantYVMin: 2,
+			wantXVMax: 7,
+			wantYVMax: 2,
+		},
+		{
+			name:      "second example: 9 steps",
+			steps:     9,
+			wantXVMin: 6,
+			wantYVMin: 3,
+			wantXVMax: 7,
+			wantYVMax: 3,
+		},
+		{
+			name:      "third example: 4 steps",
+			steps:     4,
+			wantXVMin: 7,
+			wantYVMin: -1,
+			wantXVMax: 9,
+			wantYVMax: 0,
+		},
+		{
+			name:      "winning example: 20 steps",
+			steps:     20,
+			wantXVMin: 6,
+			wantYVMin: 9,
+			wantXVMax: 7,
+			wantYVMax: 9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotXVMin, gotYVMin, gotXVMax, gotYVMax := shoot(tt.steps, 20, -10, 30, -5)
+			if gotXVMin != tt.wantXVMin {
+				t.Errorf("shoot() gotXVMin = %v, want %v", gotXVMin, tt.wantXVMin)
+			}
+			if gotYVMin != tt.wantYVMin {
+				t.Errorf("shoot() gotYVMin = %v, want %v", gotYVMin, tt.wantYVMin)
+			}
+			if gotXVMax != tt.wantXVMax {
+				t.Errorf("shoot() gotXVMax = %v, want %v", gotXVMax, tt.wantXVMax)
+			}
+			if gotYVMax != tt.wantYVMax {
+				t.Errorf("shoot() gotYVMax = %v, want %v", gotYVMax, tt.wantYVMax)
+			}
+		})
+	}
+}
+
+func TestCalcHeight(t *testing.T) {
+	tests := []struct {
+		name   string
+		steps  int
+		want   int
+		wantOK bool
+	}{
+		{
+			name:   "first example: 7,2",
+			steps:  7,
+			want:   3,
+			wantOK: true,
+		},
+		{
+			name:   "second example: 6,3",
+			steps:  9,
+			want:   6,
+			wantOK: true,
+		},
+		{
+			name:   "third example: 9,0",
+			steps:  4,
+			want:   0,
+			wantOK: true,
+		},
+		{
+			name:   "winning example: 6,9",
+			steps:  20,
+			want:   45,
+			wantOK: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotOK := calcHeight(tt.steps, 20, 30, -10, -5, map[keyT]bool{})
+			if gotOK != tt.wantOK {
+				t.Fatalf("simulate = %v, want %v", gotOK, tt.wantOK)
+			}
+			if got != tt.want {
+				t.Fatalf("simulate = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSimulate(t *testing.T) {
+	tests := []struct {
+		name   string
+		xv     int
+		yv     int
+		want   int
+		wantOK bool
+	}{
+		{
+			name:   "first example: 7,2",
+			xv:     7,
+			yv:     2,
+			want:   3,
+			wantOK: true,
+		},
+		{
+			name:   "second example: 6,3",
+			xv:     6,
+			yv:     3,
+			want:   6,
+			wantOK: true,
+		},
+		{
+			name:   "third example: 9,0",
+			xv:     9,
+			yv:     0,
+			want:   0,
+			wantOK: true,
+		},
+		{
+			name:   "fourth example: 17,-4",
+			xv:     17,
+			yv:     -4,
+			want:   0,
+			wantOK: false,
+		},
+		{
+			name:   "winning example: 6,9",
+			xv:     6,
+			yv:     9,
+			want:   45,
+			wantOK: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotOK := simulate(tt.xv, tt.yv, 20, 30, -10, -5)
+			if gotOK != tt.wantOK {
+				t.Fatalf("simulate = %v, want %v", gotOK, tt.wantOK)
+			}
+			if got != tt.want {
+				t.Fatalf("simulate = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 45\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -19,77 +200,6 @@ func BenchmarkInput(b *testing.B) {
 	test.Benchmark(b, "../input.txt", process, &logf, &printf)
 }
 
-var example1 = `target area: x=20..30, y=-10..-5
-
-.............#....#............
-.......#..............#........
-...............................
-S........................#.....
-...............................
-...............................
-...........................#...
-...............................
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................TTTTTTTT#TT
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-
-
-...............#..#............
-...........#........#..........
-...............................
-......#..............#.........
-...............................
-...............................
-S....................#.........
-...............................
-...............................
-...............................
-.....................#.........
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................T#TTTTTTTTT
-....................TTTTTTTTTTT
-
-
-S........#.....................
-.................#.............
-...............................
-........................#......
-...............................
-....................TTTTTTTTTTT
-....................TTTTTTTTTT#
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-....................TTTTTTTTTTT
-
-
-S..............................................................
-...............................................................
-...............................................................
-...............................................................
-.................#.............................................
-....................TTTTTTTTTTT................................
-....................TTTTTTTTTTT................................
-....................TTTTTTTTTTT................................
-....................TTTTTTTTTTT................................
-....................TTTTTTTTTTT..#.............................
-....................TTTTTTTTTTT................................
-...............................................................
-...............................................................
-...............................................................
-...............................................................
-................................................#..............
-...............................................................
-...............................................................
-...............................................................
-...............................................................
-...............................................................
-...............................................................
-..............................................................#
+var example1 = `
+target area: x=20..30, y=-10..-5
 `
