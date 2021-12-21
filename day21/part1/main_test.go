1c3ceb09556bfb9f2589895824bb408778d542e4
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-21 08:45:43 -0500

    Initial commit for day21

---
+++ day21/part1/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gmlewis/advent-of-code-2021/test"
+)
+
+func TestExample(t *testing.T) {
+	want := "Solution: 0\n"
+	test.Runner(t, example1, want, process, &printf)
+}
+
+func BenchmarkExample(b *testing.B) {
+	test.Benchmark(b, "../example1.txt", process, &logf, &printf)
+}
+
+func BenchmarkInput(b *testing.B) {
+	test.Benchmark(b, "../input.txt", process, &logf, &printf)
+}
+
+var example1 = `
+Player 1 starting position: 4
+Player 2 starting position: 8
+
+`
