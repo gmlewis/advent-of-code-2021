78dbf191cfa6f1e572b6cd6f3873158df3a2b125
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-22 09:19:39 -0500

    Solve day22/part1

--- day22/part1/main.go
+++ day22/part1/main.go
@@ -6,8 +6,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
+	"github.com/gmlewis/advent-of-code-2021/mathfn"
 	"github.com/gmlewis/advent-of-code-2021/must"
 )
 
@@ -22,7 +24,47 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
+	grid := Reduce(lines, gridT{}, processLine)
 
-	printf("Solution: %v\n", len(buf))
+	printf("Solution: %v\n", len(grid))
+}
+
+type keyT [3]int
+type gridT map[keyT]bool
+
+var lineRE = regexp.MustCompile(`^(\S+) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)$`)
+
+const bounds = 50
+
+func processLine(line string, acc gridT) gridT {
+	m := lineRE.FindStringSubmatch(line)
+	if len(m) != 8 {
+		log.Fatalf("unable to parse line: %v", line)
+	}
+	f := func(k keyT) { acc[k] = true }
+	if m[1] == "off" {
+		f = func(k keyT) { delete(acc, k) }
+	}
+	x1 := must.Atoi(m[2])
+	x2 := must.Atoi(m[3])
+	y1 := must.Atoi(m[4])
+	y2 := must.Atoi(m[5])
+	z1 := must.Atoi(m[6])
+	z2 := must.Atoi(m[7])
+	if mathfn.Abs(x1) > bounds || mathfn.Abs(x2) > bounds ||
+		mathfn.Abs(y1) > bounds || mathfn.Abs(y2) > bounds ||
+		mathfn.Abs(z1) > bounds || mathfn.Abs(z2) > bounds {
+		return acc
+	}
+
+	for z := z1; z <= z2; z++ {
+		for y := y1; y <= y2; y++ {
+			for x := x1; x <= x2; x++ {
+				f(keyT{x, y, z})
+			}
+		}
+	}
+
+	return acc
 }
