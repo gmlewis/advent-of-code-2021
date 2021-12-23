1f85fc8453449dd2a763b99ac074218812f3fd4f
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-12-23 17:16:54 -0500

    Found bug

--- day23/part2/main.go
+++ day23/part2/main.go
@@ -6,8 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
+	"sort"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
+	"github.com/gmlewis/advent-of-code-2021/mathfn"
 	"github.com/gmlewis/advent-of-code-2021/must"
 )
 
@@ -22,7 +25,294 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
+	p := parse(lines)
+	p = p.solve(math.MaxInt)
+	logf("\n%v", p)
 
-	printf("Solution: %v\n", len(buf))
+	printf("Solution: %v\n", p.energy)
+}
+
+type keyT [2]int
+type puzT struct {
+	energy   int
+	landings map[keyT]rune
+	inMotion map[keyT]rune
+}
+
+func (p *puzT) solve(bestEnergy int) *puzT {
+	if p.isSolved() {
+		return p
+	}
+
+	moves := p.allPossibleMoves()
+	if len(moves) == 0 {
+		return nil
+	}
+
+	// logf("solve(%v): %v allPossibleMoves: %+v", bestEnergy, p, moves)
+	var best *puzT
+	for _, move := range moves {
+		f := move.from
+		t := move.to
+		e := move.energy
+		if e+p.energy >= bestEnergy {
+			continue
+		}
+
+		// logf("Moving '%c' from %+v to %+v using %v energy", p.inMotion[f], f, t, e)
+		np := &puzT{energy: e + p.energy, landings: dup(p.landings), inMotion: dup(p.inMotion)}
+		if t[1] == 0 || arrivedX[p.inMotion[f]] != t[0] {
+			np.inMotion[t] = np.inMotion[f]
+		}
+		delete(np.inMotion, f)
+		np.landings[t] = np.landings[f]
+		delete(np.landings, f)
+
+		np = np.solve(bestEnergy)
+		if np != nil && np.energy < bestEnergy {
+			best = np
+			bestEnergy = np.energy
+			logf("NEW BEST ENERGY: %v", bestEnergy)
+		}
+	}
+	return best
+}
+
+type moveT struct {
+	from   keyT
+	to     keyT
+	energy int
+}
+
+func (p *puzT) allPossibleMoves() (moves []moveT) {
+	for k := range p.inMotion {
+		moves = append(moves, p.possibleMoves(k)...)
+	}
+	sort.Slice(moves, func(a, b int) bool {
+		if p.inMotion[moves[a].from] == p.inMotion[moves[a].from] {
+			return moves[a].energy > moves[b].energy
+		}
+		return p.inMotion[moves[a].from] < p.inMotion[moves[a].from]
+	})
+	return moves
+}
+
+func (p *puzT) possibleMoves(from keyT) (moves []moveT) {
+	r := p.inMotion[from]
+	if r == 0 {
+		log.Fatalf("possibleMoves(%+v) not in p.inMotion=%#v", from, p.inMotion)
+	}
+	roomX := 2*int(r-'A') + 2
+	if from[1] == 0 {
+		// logf("%c at %+v must move from hallway into its own roomX=%v", r, from, roomX)
+		to := keyT{roomX, 2}
+		// logf("Can %c move from %+v to %+v?", r, from, to)
+		if p.landings[to] == 0 && p.clearPath(from, to) {
+			return []moveT{{from: from, to: to, energy: energy(r, from, to)}}
+		}
+		to = keyT{roomX, 1}
+		// logf("No. Can %c move from %+v to %+v?", r, from, to)
+		if p.landings[to] == 0 && p.clearPath(from, to) {
+			return []moveT{{from: from, to: to, energy: energy(r, from, to)}}
+		}
+		// logf("No.")
+		return nil
+	}
+
+	// Moving from room into hallway.
+	if !p.clearPath(from, keyT{from[0], 0}) {
+		// logf("%c at %+v is blocked from moving into hallway", r, from)
+		return nil // blocked
+	}
+
+	// Can this be a final move into place (by going up, over, and down again)?
+	column := roomX - 1
+	if from[0] > roomX {
+		column = roomX + 1
+	}
+	// logf("Can %c move from %+v to %+v?", r, from, keyT{column, 0})
+	if p.clearPath(from, keyT{column, 0}) && (p.landings[keyT{column, 4}] == r || p.landings[keyT{column, 4}] == 0) {
+		for y := 4; y >= 1; y-- {
+			if p.clearPath(keyT{column, 0}, keyT{roomX, y}) {
+				to := keyT{roomX, y}
+				return []moveT{{from: from, to: to, energy: energy(r, from, to)}}
+			}
+		}
+	}
+
+	f := func(x int) {
+		to := keyT{x, 0}
+		if p.clearPath(from, to) {
+			moves = append(moves, moveT{from: from, to: to, energy: energy(r, from, to)})
+		}
+	}
+
+	for _, x := range orderX[from[0]] {
+		f(x)
+	}
+
+	return moves
+}
+
+var orderX = map[int][]int{
+	2: []int{1, 3, 0, 5, 7, 9, 10},
+	4: []int{3, 5, 1, 7, 0, 9, 10},
+	6: []int{5, 7, 3, 9, 10, 1, 0},
+	8: []int{7, 9, 10, 5, 3, 1, 0},
+}
+
+var arrivedX = map[rune]int{'A': 2, 'B': 4, 'C': 6, 'D': 8}
+var xToRune = map[int]rune{2: 'A', 4: 'B', 6: 'C', 8: 'D'}
+
+func (p *puzT) clearPath(from, to keyT) bool {
+	if p.landings[to] != 0 {
+		return false
+	}
+
+	if to[1] != 0 {
+		for y := 1; y < to[1]; y++ {
+			if p.landings[keyT{to[0], y}] != 0 {
+				return false
+			}
+		}
+		for x := from[0] - 1; x > to[0]; x-- {
+			if p.landings[keyT{x, 0}] != 0 {
+				return false
+			}
+		}
+		for x := from[0] + 1; x < to[0]; x++ {
+			if p.landings[keyT{x, 0}] != 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	// Moving from room into hallway.
+	for y := 1; y < from[1]; y++ {
+		if p.landings[keyT{from[0], y}] != 0 {
+			return false
+		}
+	}
+	for x := from[0] - 1; x > to[0]; x-- {
+		if p.landings[keyT{x, 0}] != 0 {
+			return false
+		}
+	}
+	for x := from[0] + 1; x < to[0]; x++ {
+		if p.landings[keyT{x, 0}] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func energy(r rune, from, to keyT) int {
+	if from[1] > 0 && to[1] > 0 {
+		dist := mathfn.Abs(from[0]-to[0]) + from[1] + to[1]
+		return energyPerStep[r] * dist
+	}
+
+	dist := mathfn.Abs(from[0]-to[0]) + mathfn.Abs(from[1]-to[1])
+	return energyPerStep[r] * dist
+}
+
+var energyPerStep = map[rune]int{
+	'A': 1,
+	'B': 10,
+	'C': 100,
+	'D': 1000,
+}
+
+func dup(src map[keyT]rune) map[keyT]rune {
+	dst := map[keyT]rune{}
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
+}
+
+func (p *puzT) isSolved() bool {
+	if len(p.landings) != 16 {
+		log.Fatalf("lost an amphipod! %v", p)
+	}
+	if len(p.inMotion) != 0 {
+		return false
+	}
+	for y := 1; y <= 4; y++ {
+		for x := 2; x <= 8; x += 2 {
+			if p.landings[keyT{x, y}] != xToRune[x] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func parse(lines []string) *puzT {
+	lines = append(lines[:4], lines[2], lines[3])
+	lines[2] = "  #D#C#B#A#"
+	lines[3] = "  #D#B#A#C#"
+	return parseLiteral(lines)
+}
+
+func parseLiteral(lines []string) *puzT {
+	p := &puzT{landings: map[keyT]rune{}, inMotion: map[keyT]rune{}}
+
+	f := func(x, y int) {
+		k := keyT{x, y}
+		r := rune(lines[y+1][x+1])
+		if r == '.' {
+			return
+		}
+		p.landings[k] = r
+		if y < 4 || arrivedX[r] != x {
+			p.inMotion[k] = r
+		}
+	}
+
+	for y := 1; y <= 4; y++ {
+		for x := 2; x <= 8; x += 2 {
+			f(x, y)
+		}
+	}
+	for x := 0; x <= 10; x++ {
+		f(x, 0)
+	}
+
+	return p
+}
+
+func (p *puzT) String() string {
+	var landings string
+	for x := 0; x <= 10; x++ {
+		r, ok := p.landings[keyT{x, 0}]
+		if ok {
+			landings += string(r)
+			continue
+		}
+		landings += "."
+	}
+
+	f := func(x, y int) rune {
+		r, ok := p.landings[keyT{x, y}]
+		if !ok {
+			return '.'
+		}
+		return r
+	}
+
+	return fmt.Sprintf(`energy=%v
+#%v#
+###%c#%c#%c#%c###
+  #%c#%c#%c#%c#
+  #%c#%c#%c#%c#
+  #%c#%c#%c#%c#
+`, p.energy, landings,
+		f(2, 1), f(4, 1), f(6, 1), f(8, 1),
+		f(2, 2), f(4, 2), f(6, 2), f(8, 2),
+		f(2, 3), f(4, 3), f(6, 3), f(8, 3),
+		f(2, 4), f(4, 4), f(6, 4), f(8, 4),
+	)
 }
--- day23/part2/main_test.go
+++ day23/part2/main_test.go
@@ -1,13 +1,186 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/test"
 )
 
+func TestPossibleMoves(t *testing.T) {
+	startPuz := parseLiteral(strings.Split(strings.TrimSpace(example1), "\n"))
+	s1 := parseLiteral(strings.Split(strings.TrimSpace(step1), "\n"))
+	if s1.inMotion[keyT{2, 4}] != 0 {
+		t.Fatalf("fatal parse: s1.inMotion[%v]=%v, want 0", keyT{2, 4}, s1.inMotion[keyT{2, 4}])
+	}
+	if s1.inMotion[keyT{6, 4}] != 0 {
+		t.Fatalf("fatal parse: s1.inMotion[%v]=%v, want 0", keyT{6, 4}, s1.inMotion[keyT{6, 4}])
+	}
+	s2 := parseLiteral(strings.Split(strings.TrimSpace(step2), "\n"))
+	s3 := parseLiteral(strings.Split(strings.TrimSpace(step3), "\n"))
+	s4 := parseLiteral(strings.Split(strings.TrimSpace(step4), "\n"))
+	s5 := parseLiteral(strings.Split(strings.TrimSpace(step5), "\n"))
+	s6 := parseLiteral(strings.Split(strings.TrimSpace(step6), "\n"))
+	s7 := parseLiteral(strings.Split(strings.TrimSpace(step7), "\n"))
+
+	tests := []struct {
+		name string
+		puz  *puzT
+		from keyT
+		want []moveT
+	}{
+		{
+			name: "first B",
+			from: keyT{2, 1},
+			want: []moveT{
+				{from: keyT{2, 1}, to: keyT{1, 0}, energy: 20},
+				{from: keyT{2, 1}, to: keyT{3, 0}, energy: 20},
+				{from: keyT{2, 1}, to: keyT{0, 0}, energy: 30},
+				{from: keyT{2, 1}, to: keyT{5, 0}, energy: 40},
+				{from: keyT{2, 1}, to: keyT{7, 0}, energy: 60},
+				{from: keyT{2, 1}, to: keyT{9, 0}, energy: 80},
+				{from: keyT{2, 1}, to: keyT{10, 0}, energy: 90},
+			},
+		},
+		{
+			name: "first C",
+			from: keyT{4, 1},
+			want: []moveT{
+				{from: keyT{4, 1}, to: keyT{3, 0}, energy: 200},
+				{from: keyT{4, 1}, to: keyT{5, 0}, energy: 200},
+				{from: keyT{4, 1}, to: keyT{1, 0}, energy: 400},
+				{from: keyT{4, 1}, to: keyT{7, 0}, energy: 400},
+				{from: keyT{4, 1}, to: keyT{0, 0}, energy: 500},
+				{from: keyT{4, 1}, to: keyT{9, 0}, energy: 600},
+				{from: keyT{4, 1}, to: keyT{10, 0}, energy: 700},
+			},
+		},
+		{
+			name: "second B",
+			from: keyT{6, 1},
+			want: []moveT{
+				{from: keyT{6, 1}, to: keyT{5, 0}, energy: 20},
+				{from: keyT{6, 1}, to: keyT{7, 0}, energy: 20},
+				{from: keyT{6, 1}, to: keyT{3, 0}, energy: 40},
+				{from: keyT{6, 1}, to: keyT{9, 0}, energy: 40},
+				{from: keyT{6, 1}, to: keyT{10, 0}, energy: 50},
+				{from: keyT{6, 1}, to: keyT{1, 0}, energy: 60},
+				{from: keyT{6, 1}, to: keyT{0, 0}, energy: 70},
+			},
+		},
+		{
+			name: "first D",
+			from: keyT{8, 1},
+			want: []moveT{
+				{from: keyT{8, 1}, to: keyT{7, 0}, energy: 2000},
+				{from: keyT{8, 1}, to: keyT{9, 0}, energy: 2000},
+				{from: keyT{8, 1}, to: keyT{10, 0}, energy: 3000},
+				{from: keyT{8, 1}, to: keyT{5, 0}, energy: 4000},
+				{from: keyT{8, 1}, to: keyT{3, 0}, energy: 6000},
+				{from: keyT{8, 1}, to: keyT{1, 0}, energy: 8000},
+				{from: keyT{8, 1}, to: keyT{0, 0}, energy: 9000},
+			},
+		},
+		{
+			name: "blocked D",
+			from: keyT{4, 2},
+		},
+		{
+			name: "second blocked A",
+			from: keyT{8, 2},
+		},
+		{
+			name: "A moves to left",
+			puz:  s1,
+			from: keyT{8, 2},
+			want: []moveT{
+				{from: keyT{8, 2}, to: keyT{7, 0}, energy: 3},
+				{from: keyT{8, 2}, to: keyT{9, 0}, energy: 3},
+				{from: keyT{8, 2}, to: keyT{5, 0}, energy: 5},
+				{from: keyT{8, 2}, to: keyT{3, 0}, energy: 7},
+				{from: keyT{8, 2}, to: keyT{1, 0}, energy: 9},
+				{from: keyT{8, 2}, to: keyT{0, 0}, energy: 10},
+			},
+		},
+		{
+			name: "B moves to right",
+			puz:  s2,
+			from: keyT{6, 1},
+			want: []moveT{
+				{from: keyT{6, 1}, to: keyT{5, 0}, energy: 20},
+				{from: keyT{6, 1}, to: keyT{7, 0}, energy: 20},
+				{from: keyT{6, 1}, to: keyT{3, 0}, energy: 40},
+				{from: keyT{6, 1}, to: keyT{9, 0}, energy: 40},
+				{from: keyT{6, 1}, to: keyT{1, 0}, energy: 60},
+			},
+		},
+		{
+			name: "next B moves to right",
+			puz:  s3,
+			from: keyT{6, 2},
+			want: []moveT{
+				{from: keyT{6, 2}, to: keyT{5, 0}, energy: 30},
+				{from: keyT{6, 2}, to: keyT{7, 0}, energy: 30},
+				{from: keyT{6, 2}, to: keyT{3, 0}, energy: 50},
+				{from: keyT{6, 2}, to: keyT{1, 0}, energy: 70},
+			},
+		},
+		{
+			name: "next A moves to left",
+			puz:  s4,
+			from: keyT{6, 3},
+			want: []moveT{
+				{from: keyT{6, 3}, to: keyT{5, 0}, energy: 4},
+				{from: keyT{6, 3}, to: keyT{3, 0}, energy: 6},
+				{from: keyT{6, 3}, to: keyT{1, 0}, energy: 8},
+			},
+		},
+		{
+			name: "C moves into place",
+			puz:  s5,
+			from: keyT{4, 1},
+			want: []moveT{
+				{from: keyT{4, 1}, to: keyT{6, 3}, energy: 600},
+			},
+		},
+		{
+			name: "next C moves into place",
+			puz:  s6,
+			from: keyT{4, 2},
+			want: []moveT{
+				{from: keyT{4, 2}, to: keyT{6, 2}, energy: 600},
+			},
+		},
+		{
+			name: "next B moves into hallway",
+			puz:  s7,
+			from: keyT{4, 3},
+			want: []moveT{
+				{from: main.keyT{4, 3}, to: main.keyT{5, 0}, energy: 50},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.puz == nil {
+				tt.puz = startPuz
+			}
+			got := tt.puz.possibleMoves(tt.from)
+			if len(got) != len(tt.want) {
+				t.Errorf("possibleMoves(%v) = %#v, want %#v", tt.from, got, tt.want)
+			}
+			for i := range got {
+				if got[i].from != tt.want[i].from || got[i].to != tt.want[i].to || got[i].energy != tt.want[i].energy {
+					t.Errorf("possibleMoves(%v)[%v] = %#v, want %#v", tt.from, i, got[i], tt.want[i])
+				}
+			}
+		})
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 44169\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -23,70 +196,238 @@ var example1 = `
 #############
 #...........#
 ###B#C#B#D###
+  #D#C#B#A#
+  #D#B#A#C#
   #A#D#C#A#
   #########
+`
 
-
+var step1 = `
 #############
-#...........#
-###A#B#C#D###
-  #A#B#C#D#
-  #########
-
-
-#############
-#...........#
-###B#C#B#D###
+#..........D#
+###B#C#B#.###
+  #D#C#B#A#
+  #D#B#A#C#
   #A#D#C#A#
   #########
+`
 
-
+var step2 = `
 #############
-#...B.......#
-###B#C#.#D###
+#A.........D#
+###B#C#B#.###
+  #D#C#B#.#
+  #D#B#A#C#
   #A#D#C#A#
   #########
+`
 
-
+var step3 = `
 #############
-#...B.......#
-###B#.#C#D###
+#A........BD#
+###B#C#.#.###
+  #D#C#B#.#
+  #D#B#A#C#
   #A#D#C#A#
   #########
+`
 
-
+var step4 = `
 #############
-#.....D.....#
-###B#.#C#D###
+#A......B.BD#
+###B#C#.#.###
+  #D#C#.#.#
+  #D#B#A#C#
+  #A#D#C#A#
+  #########
+`
+
+var step5 = `
+#############
+#AA.....B.BD#
+###B#C#.#.###
+  #D#C#.#.#
+  #D#B#.#C#
+  #A#D#C#A#
+  #########
+`
+
+var step6 = `
+#############
+#AA.....B.BD#
+###B#.#.#.###
+  #D#C#.#.#
+  #D#B#C#C#
+  #A#D#C#A#
+  #########
+`
+
+var step7 = `
+#############
+#AA.....B.BD#
+###B#.#.#.###
+  #D#.#C#.#
+  #D#B#C#C#
+  #A#D#C#A#
+  #########
+`
+
+var step8 = `
+#############
+#AA...B.B.BD#
+###B#.#.#.###
+  #D#.#C#.#
+  #D#.#C#C#
+  #A#D#C#A#
+  #########
+`
+
+var step9 = `
+#############
+#AA.D.B.B.BD#
+###B#.#.#.###
+  #D#.#C#.#
+  #D#.#C#C#
+  #A#.#C#A#
+  #########
+`
+
+var step10 = `
+#############
+#AA.D...B.BD#
+###B#.#.#.###
+  #D#.#C#.#
+  #D#.#C#C#
   #A#B#C#A#
   #########
+`
 
-
+var step11 = `
 #############
-#.....D.....#
-###.#B#C#D###
+#AA.D.....BD#
+###B#.#.#.###
+  #D#.#C#.#
+  #D#B#C#C#
   #A#B#C#A#
   #########
+`
 
-
+var step12 = `
 #############
-#.....D.D.A.#
-###.#B#C#.###
+#AA.D......D#
+###B#.#.#.###
+  #D#B#C#.#
+  #D#B#C#C#
+  #A#B#C#A#
+  #########
+`
+
+var step13 = `
+#############
+#AA.D......D#
+###B#.#C#.###
+  #D#B#C#.#
+  #D#B#C#.#
+  #A#B#C#A#
+  #########
+`
+
+var step14 = `
+#############
+#AA.D.....AD#
+###B#.#C#.###
+  #D#B#C#.#
+  #D#B#C#.#
   #A#B#C#.#
   #########
+`
 
-
+var step15 = `
 #############
-#.........A.#
-###.#B#C#D###
+#AA.......AD#
+###B#.#C#.###
+  #D#B#C#.#
+  #D#B#C#.#
   #A#B#C#D#
   #########
+`
 
+var step16 = `
+#############
+#AA.......AD#
+###.#B#C#.###
+  #D#B#C#.#
+  #D#B#C#.#
+  #A#B#C#D#
+  #########
+`
 
+var step17 = `
+#############
+#AA.......AD#
+###.#B#C#.###
+  #.#B#C#.#
+  #D#B#C#D#
+  #A#B#C#D#
+  #########
+`
+
+var step18 = `
+#############
+#AA.D.....AD#
+###.#B#C#.###
+  #.#B#C#.#
+  #.#B#C#D#
+  #A#B#C#D#
+  #########
+`
+
+var step19 = `
+#############
+#A..D.....AD#
+###.#B#C#.###
+  #.#B#C#.#
+  #A#B#C#D#
+  #A#B#C#D#
+  #########
+`
+
+var step20 = `
+#############
+#...D.....AD#
+###.#B#C#.###
+  #A#B#C#.#
+  #A#B#C#D#
+  #A#B#C#D#
+  #########
+`
+
+var step21 = `
+#############
+#.........AD#
+###.#B#C#.###
+  #A#B#C#D#
+  #A#B#C#D#
+  #A#B#C#D#
+  #########
+`
+
+var step22 = `
+#############
+#..........D#
+###A#B#C#.###
+  #A#B#C#D#
+  #A#B#C#D#
+  #A#B#C#D#
+  #########
+`
+
+var step23 = `
 #############
 #...........#
 ###A#B#C#D###
   #A#B#C#D#
+  #A#B#C#D#
+  #A#B#C#D#
   #########
-
 `
