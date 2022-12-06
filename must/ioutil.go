b3b745ba9e8f13d92a087d2c92f8bea276203363
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-06 08:34:55 -0500

    Add ReadSplitFile
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- must/ioutil.go
+++ must/ioutil.go
@@ -6,17 +6,23 @@ import (
 )
 
 // ReadFile reads a file as a string or causes a fatal error.
+// It trims extra trailing whitespace from the string.
 func ReadFile(filename string) string {
 	buf, err := os.ReadFile(filename)
 	if err != nil {
 		fatal(err)
 	}
 
-	return string(buf)
+	return strings.TrimRight(string(buf), " \t\n")
 }
 
 // ReadFileLines reads a file and returns a slice of lines.
 func ReadFileLines(filename string) []string {
+	return ReadSplitFile(filename, "\n")
+}
+
+// ReadSplitFile reads a file and splits on the given string.
+func ReadSplitFile(filename, split string) []string {
 	s := ReadFile(filename)
-	return strings.Split(s, "\n")
+	return strings.Split(s, split)
 }
